3507f72f4adbfb7359da487a16dfefc90f4a249a
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2023-03-02 08:18:36 +0000

    Created array of Courses and Check for particular one

--- Demo/index.js
+++ Demo/index.js
@@ -1,5 +1,29 @@
 const app = require("express")();
 
+const course = [
+    {
+        id: 1,
+        name:"Java"
+    },
+    {
+        id: 2,
+        name:"C++"
+    },
+    {
+        id: 3,
+        name:"Full Stack"
+    },
+    {
+        id: 4,
+        name:"Android"
+    },
+    {
+        id: 5,
+        name:"DEVOPS"
+    },
+]
+
+
 app.get('/', (req, res) => {
     res.send("Hello World!!")
 })
@@ -9,7 +33,9 @@ app.get('/api/courses', (req, res) => {
 })
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
+    const c = course.find(cid => cid.id === parseInt(req.params.id));
+    if(!c) res.status(404).send("Course with given id is not available!!")
+    res.send(`Course name is:${c.name} and it's id is: ${c.id}`)
 })
 
 
