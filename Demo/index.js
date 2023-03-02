56829f6f69aafdaec9f56b8a4260cf9bfb122a1c
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2023-03-02 07:26:53 +0000

    Root of URL at line 3

--- Demo/index.js
+++ Demo/index.js
@@ -1,3 +1,11 @@
 const app = require("express")();
 
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
 
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3])
+})
+
+app.listen(3000,()=> console.log("Server is running on Port 5000"))
